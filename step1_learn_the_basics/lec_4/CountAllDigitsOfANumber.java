ecdf5ccd8d187da8cc94d52cafa7b294d14fbe01
Anvi Mittal <b241130@skit.ac.in>
2026-07-15 19:43:36 +0530

    count digits

---
+++ step1_learn_the_basics/lec_4/CountAllDigitsOfANumber.java
@@ -0,0 +1,18 @@
+package step1_learn_the_basics.lec_4;
+
+public class CountAllDigitsOfANumber {
+    public int countDigit(int n){
+        if(n==0) return 1;
+        int count = 0;
+        while(n>0){
+            count++;
+            n = n/10;
+        }
+        return count;
+    }
+
+    public static void main(String args[]){
+        CountAllDigitsOfANumber num = new CountAllDigitsOfANumber();
+        num.countDigit(45);
+    }
+}
