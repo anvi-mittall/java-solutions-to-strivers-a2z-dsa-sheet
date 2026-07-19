71402ef279c2977ca847033e8b940dd15d999f80
Anvi Mittal <b241130@skit.ac.in>
2026-07-19 19:53:04 +0530

    fibonacciNum

---
+++ step1_learn_the_basics/lec_5/FibonacciNum.java
@@ -0,0 +1,20 @@
+package step1_learn_the_basics.lec_5;
+
+public class FibonacciNum{
+    public int fib(int n){
+        if(n==0){
+            return 0;
+        }
+
+        if(n==1){
+            return 1;
+        }
+
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String args[]){
+        FibonacciNum f = new FibonacciNum();
+        f.fib(4);
+    }
+}
