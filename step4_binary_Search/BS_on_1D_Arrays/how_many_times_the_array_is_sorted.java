e37764abbf6b63888810809e3cfdbe34aab37f22
Anvi Mittal <b241130@skit.ac.in>
2026-08-21 19:01:23 +0530

    howManyTimesTheArrayIsSorted

--- step4_binary_Search/BS_on_1D_Arrays/how_many_times_the_array_is_sorted.java
+++ step4_binary_Search/BS_on_1D_Arrays/how_many_times_the_array_is_sorted.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class how_many_times_the_array_is_sorted {
-    public int findMin(ArrayList<Integer> nums) {
+    public int findKRotation(ArrayList<Integer> nums) {
         int low = 0;
         int high = nums.size() - 1;
         int minIndex = 0;
-        int minVal = nums.get(0);
+        int minValue = nums.get(0);
 
         while (low < high) {
             int mid = low + (high - low) / 2;
 
             if (nums.get(mid) > nums.get(mid + 1)) {
                 minIndex = mid + 1;
-                minVal = nums.get(mid + 1);
+                minValue = nums.get(mid + 1);
                 break;
             } else if(nums.get(low) < nums.get(mid)) {
                 low = mid + 1;
@@ -30,7 +30,7 @@ public class how_many_times_the_array_is_sorted {
     public static void main(String[] args) {
         how_many_times_the_array_is_sorted obj = new how_many_times_the_array_is_sorted();
         ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(4, 5, 6, 7, 0, 1, 2));
-        int rotations = obj.findMin(nums);
+        int rotations = obj.findKRotation(nums);
         System.out.println(rotations);
     }
 }
